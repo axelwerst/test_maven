e54b6d99d1e6279d9106e158c1ab4ecc36f0a1e6
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-10-24 08:20:52 +0200

    pusto

--- src/main/java/com/gmail/axelwerst/App.java
+++ src/main/java/com/gmail/axelwerst/App.java
@@ -9,22 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "С початку потрібно зібрати інформацію про автівку" );
-        System.out.println("скільки палива у баці?");
-        Scanner scanner = new Scanner(System.in);
-        float bakR = scanner.nextFloat();
-        System.out.println("Введіть дату проходження техогляду рік + місяц + день");
-        LocalDate tuv = LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-        LocalDate tuvNew = tuv.plusYears(2);
-        LocalDate ld = LocalDate.now();
-        if (ld.isAfter(tuvNew)){
-        System.out.println("їхати не можна, треба пройти техогляд");
-}
-        System.out.println("наступний техогляд " +tuvNew);
 
-        float bak = Wagen.BAK(bakR);
-        float kmLeft = Wagen.probeg(bakR);
-        System.out.println("бак повен на "+bak+" %");
-        System.out.println("ще можно проїхати " +kmLeft+ " кілометрів");
     }
 }
--- src/main/java/com/gmail/axelwerst/Wagen.java
+++ src/main/java/com/gmail/axelwerst/Wagen.java
@@ -3,28 +3,6 @@ package com.gmail.axelwerst;
 public class Wagen {
 
 
-public static float probeg (float bakR){
-   if (bakR <= 0) {
-       System.out.println("їхати не можна");
-       return 0;
-   }
-       if (bakR <= 7){
-       System.out.println("треба на заправку");
-   }
-   if (bakR>50){
-       System.out.println("паливо льється через край");
-   }
-    int ras =7;
-    float kmLeft =(float) bakR/ras * 100;
-    return  kmLeft;
-}
-public static float BAK (float bakR) {
-    if (bakR <= 0) {
-        System.out.println("їхати не можна");
-        return 0;
-    }
-    int bakMax = 50;
-    float bak = (float) bakR /bakMax*100;
-    return  bak;
+
 }
 }
