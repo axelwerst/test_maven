818f9a10b82743d1eb89b6989a0a0caf02404c4e
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-11-06 11:46:17 +0100

    +test

--- src/main/java/com/gmail/axelwerst/Wagen.java
+++ src/main/java/com/gmail/axelwerst/Wagen.java
@@ -1,8 +1,35 @@
 package com.gmail.axelwerst;
 
 public class Wagen {
+    private int bakReal;
 
+    public Wagen(int bakReal) {
+        this.bakReal = bakReal;
+    }
 
+    public float probeg() {
+        if (this.bakReal <= 0) {
+            System.out.println("їхати не можна");
+            return 0;
+        }
+        if (this.bakReal <= 7) {
+            System.out.println("треба на заправку");
+        }
+        if (this.bakReal > 50) {
+            System.out.println("паливо льється через край");
+        }
+        int ras = 7;
+        float kmLeft = (float) this.bakReal / ras * 100;
+        return kmLeft;
+    }
+    public float BAK() {
+        if (this.bakReal <= 0) {
+            System.out.println("їхати не можна");
+            return 0;
+        }
+        int bakMax = 50;
+        float bak = (float) this.bakReal / bakMax * 100;
+        return bak;
+    }
 
-}
 }
